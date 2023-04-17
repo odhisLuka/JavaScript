09c899484dc74befb3f11784d2bf1ead6151460c
odhisLuka <luketrevor03@gmail.com>
2023-04-17 18:31:00 +0300

    styling in DOM

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -1,15 +1,16 @@
-const h1 = document.getElementById('main-title');
+const section = document.querySelector('section');
+const button = document.querySelector('button');
 
-h1.textContent = "Some new title";
-h1.style.color = 'white';
-h1.style.backgroundColor = 'black';
+// section.style.backgroundColor = 'blue';
 
-const Li = document.querySelector('li:last-of-type');
-Li.textContent = Li.textContent + ' Changed!';
-
-//const listItemElements = document.querySelectorAll(li);
-const listItemElements = document.getElementsByTagName('li');
-
-for (liItemEl of listItemElements) {
-  console.dir(liItemEl);
-} 
+button.addEventListener('click', () => {
+  // if (section.className === "red-bg visible") {
+  //   section.className = "red-bg invisible";
+  // } else {
+  //   section.className = "red-bg visible";
+  // }
+  
+  // section.classList.toggle("visible");
+  
+  section.classList.toggle('invisible');
+});
