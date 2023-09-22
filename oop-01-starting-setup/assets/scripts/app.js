ac90a21883d961f943d5482ea10bd08dc35c1e7c
odhisLuka <luketrevor03@gmail.com>
2023-09-22 22:36:56 +0300

    outsourcing logic for a signle product item

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,9 +1,33 @@
+// groups together product details
 class Product {
   constructor(title, image, price, desc) {
     this.title = title;
     this.imageUrl = image;
     this.price = price;
     this.description = desc;
+  }
+}
+// Generating HTML single product item
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const productItemElement = document.createElement("li");
+    productItemElement.className = "product-item";
+    productItemElement.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl} alt="${this.product.title}">
+          <div class=".this.product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to cart</button>
+          </div>
+        </div>
+    `;
+    return productItemElement;
   }
 }
 class ProductList {
@@ -24,29 +48,18 @@ class ProductList {
 
   constructor() {}
 
-  renderHTML() {
+  generateHTML() {
     const productContainer = document.getElementById("app");
     const productList = document.createElement("ul");
     productList.className = "product-list";
 
     for (const product of this.products) {
-      const productItem = document.createElement("li");
-      productItem.className = "product-item";
-      productItem.innerHTML = `
-        <div>
-          <img src="${product.imageUrl}">
-          <div class=".product-item__content">
-            <h2>${product.title}</h2>
-            <h3>\$${product.price}</h3>
-            <p>${product.description}</p>
-            <button>Add to cart</button>
-          </div>
-        </div>
-    `;
-      productList.append(productItem);
+      const productItem = new ProductItem(product);
+      const productItemElement = productItem.render();
+      productList.append(productItemElement);
     }
     productContainer.append(productList);
   }
 }
 const myProductList = new ProductList();
-myProductList.renderHTML();
+myProductList.generateHTML();
